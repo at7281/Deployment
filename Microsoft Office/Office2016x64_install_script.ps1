815ecbb23996d35e837a9303b8170e30543a66f1
at7281 <at7281@gmail.com>
2021-12-28 09:55:24 +0530

    Update Office2016x64_install_script.ps1

--- Microsoft Office/Office2016x64_install_script.ps1
+++ Microsoft Office/Office2016x64_install_script.ps1
@@ -3,14 +3,14 @@ mkdir Office2016x64
 curl https://git.io/Jy0vT -o Office2016x64.zip
 Expand-Archive C:\Users\Public\Office2016x64.zip -DestinationPath C:\Users\Public\Office2016x64
 Invoke-Item C:\Users\Public\Office2016x64\install.cmd
-pause >nul
+pause
 cd C:\Users\Public
 curl https://git.io/Jy0vV -o Office2016_act.cmd
 Invoke-Item C:\Users\Public\Office2016_act.cmd
-pause >nul
+pause
 Echo Cleaning up...
 cd $HOME
 rm C:\Users\Public\Office2016x64 -r -fo
 rm C:\Users\Public\Office2016x64.zip -r -fo
 rm C:\Users\Public\Office2016_act.cmd -r -fo
-pause >nul
+pause
