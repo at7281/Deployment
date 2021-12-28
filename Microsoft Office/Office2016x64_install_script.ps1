bfd69ec34f3c6022e2bea7b318776ea387afc8ac
at7281 <at7281@gmail.com>
2021-12-28 10:15:44 +0530

    remove null

--- Microsoft Office/Office2016x64_install_script.ps1
+++ Microsoft Office/Office2016x64_install_script.ps1
@@ -3,4 +3,4 @@ mkdir Office2016x64
 curl https://git.io/Jy0vT -o Office2016x64.zip
 Expand-Archive C:\Users\Public\Office2016x64.zip -DestinationPath C:\Users\Public\Office2016x64
 Invoke-Item C:\Users\Public\Office2016x64\install.cmd
-pause >nul
+pause
