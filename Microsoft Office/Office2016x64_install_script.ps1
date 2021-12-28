e5fd7c615c076d5bd403cf110ffac932f061f179
at7281 <at7281@gmail.com>
2021-12-28 09:51:53 +0530

    c2

---
+++ Microsoft Office/Office2016x64_install_script.ps1
@@ -0,0 +1,16 @@
+cd C:\Users\Public
+mkdir Office2016x64
+curl https://git.io/Jy0vT -o Office2016x64.zip
+Expand-Archive C:\Users\Public\Office2016x64.zip -DestinationPath C:\Users\Public\Office2016x64
+Invoke-Item C:\Users\Public\Office2016x64\install.cmd
+pause >nul
+cd C:\Users\Public
+curl https://git.io/Jy0vV -o Office2016_act.cmd
+Invoke-Item C:\Users\Public\Office2016_act.cmd
+pause >nul
+Echo Cleaning up...
+cd $HOME
+rm C:\Users\Public\Office2016x64 -r -fo
+rm C:\Users\Public\Office2016x64.zip -r -fo
+rm C:\Users\Public\Office2016_act.cmd -r -fo
+pause >nul
