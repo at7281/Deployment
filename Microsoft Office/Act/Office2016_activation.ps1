5b541321adedd69cdc3da78a83af43770ae498aa
at7281 <96759494+at7281@users.noreply.github.com>
2022-01-01 10:47:39 +0530

    Update Office2016_activation.ps1

--- Microsoft Office/Act/Office2016_activation.ps1
+++ Microsoft Office/Act/Office2016_activation.ps1
@@ -1,3 +1,5 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
 cd C:\Users\Public
 curl https://git.io/Jy0vV -o Office2016_act.cmd
 Invoke-Item C:\Users\Public\Office2016_act.cmd
