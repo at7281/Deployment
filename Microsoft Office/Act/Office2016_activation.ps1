e55dcc44aed197eaa160182faf3330334aa5770f
Aman Tiwari <amantiwari216@gmail.com>
2024-09-12 11:58:46 +0530

    Removed unnecessary MS items

--- Microsoft Office/Act/Office2016_activation.ps1
+++
@@ -1,4 +0,0 @@
-cd C:\Users\Public
-curl https://git.io/Jy0vV -o Office2016_act.cmd
-Invoke-Item C:\Users\Public\Office2016_act.cmd
-pause
--- Microsoft Office/Act/Office2019_act.ps1
+++
@@ -1,4 +0,0 @@
-cd C:\Users\Public
-curl https://raw.githubusercontent.com/at7281/Deployment/main/Microsoft%20Office/Act/Office2019_act.cmd -o office_2019_act.cmd
-Invoke-Item C:\Users\Public\office_2019_act.cmd
-pause
